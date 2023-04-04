059142e43617fdb8306eb9f4635c0c83cf050431
Shukla <s.rahulelite@gmail.com>
2023-04-05 02:16:31 +0530

    Inititated Database Things

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
+app.config["SECRET_KEY"] = '571ebfo2784rjsd'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///doctor_patient_info.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 
 from app import views
 
