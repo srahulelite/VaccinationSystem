dc7d6094108d7586460b910f59b54ba3fc0cbbb8
Shukla <s.rahulelite@gmail.com>
2023-04-05 02:37:46 +0530

    model completed

--- app/models.py
+++ app/models.py
@@ -1,2 +1,35 @@
 from app import db
+from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
 
+class Doctor:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    phone_number = db.Column(db.String(12), unique=True, nullable=False)
+    government_number = db.Column(db.String(6), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    patients = db.relationship('Patient', backref='doctor', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Patient:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    phone_number = db.Column(db.String(12), unique=True, nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'))
+    vaccine_doses = db.relationship('Vaccine_Dose', backref='patient')
+
+class Vaccine_Dose:
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(120), nullable=False)
+    volume = db.Column(db.numeric, nullable=False)
+    dose_no = db.Column(db.Integer, nullable=False)
+    dose_id = db.Column(db.String(6), nullable=False)
+    dose_dte = db.Column(db.DateTime(timezone=True), default=datetime.now)
+    patient_id = db.Column(db.Integer, db.ForiegnKey('patient.id'))
