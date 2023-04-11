f64e4499c5fc4314082d075cbd916c3d2f9e8e9b
Shukla <s.rahulelite@gmail.com>
2023-04-11 07:25:04 +0530

    DB migration Done

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 app.config["SECRET_KEY"] = '571ebfo2784rjsd'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///doctor_patient_info.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:@localhost/doctor_patient_info'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 app.app_context().push()
 
 app.config.from_object('config')
